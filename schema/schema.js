51752dd76cf1313fd2c405fd48325a1f9a5c022a
Zoltan Toth <contact@zoltantoth.com>
2018-04-04 21:27:15 -0400

    mutation added - deleteUser

--- schema/schema.js
+++ schema/schema.js
@@ -86,6 +86,16 @@ const mutation = new GraphQLObjectType({
                 return axios.post(`http://localhost:3000/users`, { firstName, age })
                     .then(resp => resp.data)
             }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3000/users/${id}`)
+                    .then(resp => resp.data)
+            }
         }
     }
 })
