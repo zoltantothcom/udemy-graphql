b53c72cd26d78d3e79547dd2322cd093f1ca4eb8
Zoltan Toth <contact@zoltantoth.com>
2018-04-06 07:57:56 -0400

    SongCreate component added and wired up to Router

---
+++ client/components/SongCreate.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: ''
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create a New Song</h3>
+                <form>
+                    <label>Song Title:</label>
+                    <input 
+                        onChange={e => this.setState({ title: e.target.value })}
+                        value={this.state.title} />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SongCreate;
