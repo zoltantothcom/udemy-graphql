e74b43b8f6152059da92adc4077d03c78a7995b3
Zoltan Toth <contact@zoltantoth.com>
2018-04-06 09:32:02 -0400

    passing query variables from component to gql

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
     constructor(props) {
@@ -7,13 +9,25 @@ class SongCreate extends Component {
         this.state = {
             title: ''
         };
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        this.props.mutate({
+            variables: {
+                title: this.state.title
+            }
+        });
     }
 
     render() {
         return (
             <div>
                 <h3>Create a New Song</h3>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <label>Song Title:</label>
                     <input 
                         onChange={e => this.setState({ title: e.target.value })}
@@ -24,4 +38,13 @@ class SongCreate extends Component {
     }
 }
 
-export default SongCreate;
+const mutation = gql`
+    mutation addSong($title: String) {
+        addSong(title: $title) {
+            id
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
