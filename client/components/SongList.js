6b063041de46a227053cbd82a568352c88ec0b16
Zoltan Toth <contact@zoltantoth.com>
2018-04-06 10:49:34 -0400

    Deleting a song by mutation + simple  refetch

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 
 import query from '../queries/fetchSongs';
+import mutation from '../queries/deleteSong';
 
 class SongList extends Component {
+    onSongDelete(id) {
+        this.props.mutate({ variables: { id } })
+            .then(() => this.props.data.refetch());
+    }
+
     renderSongs() {
-        return this.props.data.songs.map(song => {
+        return this.props.data.songs.map(({ id, title }) => {
             return (
-                <li key={song.id} className="collection-item">
-                    {song.title}
+                <li key={id} className="collection-item">
+                    {title}
+                    <i 
+                        className="material-icons right"
+                        onClick={() => this.onSongDelete(id)}
+                    >
+                        delete
+                    </i>
                 </li>
             )
         })
@@ -36,4 +47,6 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(
+    graphql(query)(SongList)
+);
--- client/index.js
+++ client/index.js
@@ -4,6 +4,8 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import './style/style.css';
+
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
