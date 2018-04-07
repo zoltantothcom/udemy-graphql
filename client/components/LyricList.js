b4a0860ff17780b6d0c425b1ead1496b6ebef329
Zoltan Toth <contact@zoltantoth.com>
2018-04-07 00:15:53 -0400

    Like lyric mutation with optimistic UI updates

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import mutation from '../queries/likeLyric';
 
 class LyricList extends Component {
+    onLike(id, likes) {
+        this.props.mutate({ 
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        });
+    }
+
     renderLyrics() {
-        return this.props.lyrics.map(({ id, content }) => {
+        return this.props.lyrics.map(({ id, content, likes }) => {
             return (
                 <li className="collection-item" key={id}>
                     {content}
+                    <i 
+                        className="material-icons right"
+                        onClick={() => this.onLike(id, likes)}
+                    >
+                        thumb_up
+                    </i>
+                    <span className="right">{likes}</span>
                 </li>
             );
         });
@@ -20,4 +44,4 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
